a1621d665f563469b4f54ca8e5586b083cee9633
Bhamjee <bhamjeei@legal.regn.net>
2024-09-23 14:02:21 +0200

    converting js data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { from, of } from 'rxjs';
+import { from, fromEvent, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +15,41 @@ export class AppComponent {
         // set up observable array with data
         // naming convention is to add a '$' at the end of the name
         const numbers$ = from([1,2,3,4,5]);
+        const users = [
+            {id: '1', name: 'John', age: 30},
+            {id: '2', name: 'Jack', age: 35},
+            {id: '3', name: 'Mike', age: 40},
+        ];
+
+        const messagePromise = new Promise((resolve) => {
+            setTimeout(() => {
+                resolve('Promise resolved!');
+            }, 1000)
+        })
+
+        const users$ = of(users);
+        /** important to use from when dealing with promises
+         *  as 'from' returns the value once the promise has resolved
+         *  whereas 'of' returns the promise itself and not the resolved value
+         * */
+        const message$ = from(messagePromise);
+        const bodyClicks$ = fromEvent(document, 'click');
 
         numbers$.subscribe((data) => {
             console.log('subscriber', data);
             this.data = data;
+        })
+
+        users$.subscribe((users) => {
+            console.log('users', users)
+        })
+
+        message$.subscribe((message) => {
+            console.log('message', message)
+        })
+
+        bodyClicks$.subscribe((event) => {
+            console.log('event', event)
         })
     }
 }
