d070f88e0a1f7ab5de8a9749b7435f78b8dd97a0
Bhamjee <bhamjeei@legal.regn.net>
2024-09-23 13:47:08 +0200

    intro to observables and subscribers as well as  of and from operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+    data: number = 0;
+
+    constructor() {
+        // set up observable array with data
+        // naming convention is to add a '$' at the end of the name
+        const numbers$ = from([1,2,3,4,5]);
+
+        numbers$.subscribe((data) => {
+            console.log('subscriber', data);
+            this.data = data;
+        })
+    }
 }
